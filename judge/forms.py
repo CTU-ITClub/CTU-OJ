e3fb922e7f21ca0d3a8936a3e39a376a35b7739b
Le Van Dat <72507371+lvdat@users.noreply.github.com>
2023-08-28 22:33:42 +0700

    add EditOrganizationForm class
    add EditOrganizationForm class

--- judge/forms.py
+++ judge/forms.py
@@ -134,6 +134,13 @@ class ProposeProblemSolutionForm(ModelForm):
             'publish_on': DateInput(attrs={'type': 'date'}),
         }
 
+class EditOrganizationForm(ModelForm):
+    class Meta:
+        model = Organization
+        fields = ['about', 'logo_override_image', 'admins']
+        widgets = {'admins': Select2MultipleWidget(attrs={'style': 'width: 200px'})}
+        if HeavyPreviewPageDownWidget is not None:
+            widgets['about'] = HeavyPreviewPageDownWidget(preview=reverse_lazy('organization_preview'))
 
 class LanguageLimitForm(ModelForm):
     def __init__(self, *args, **kwargs):
