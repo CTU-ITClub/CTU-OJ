464aaa1119f749b00f6b188640291d41f825f7f6
Le Van Dat <72507371+lvdat@users.noreply.github.com>
2023-08-29 15:21:19 +0700

    Remove 'Count' import to fix lint

--- judge/views/api/api_v2.py
+++ judge/views/api/api_v2.py
@@ -2,7 +2,7 @@ from operator import attrgetter
 
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist, PermissionDenied, ValidationError
-from django.db.models import Count, F, OuterRef, Prefetch, Q, Subquery
+from django.db.models import F, OuterRef, Prefetch, Q, Subquery
 from django.http import Http404, JsonResponse
 from django.utils import timezone
 from django.utils.functional import cached_property
